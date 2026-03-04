71768641d68f03920afb331f73766ba5211e3013
jorgebelr <jorgebelr99@gmail.com>
2026-03-04 11:09:44 -0600

    Initial Commit

---
+++ Carnet+/Carnet_App.swift
@@ -0,0 +1,17 @@
+//
+//  Carnet_App.swift
+//  Carnet+
+//
+//  Created by Jorge Beltrán on 04/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Carnet_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Carnet+/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Carnet+
+//
+//  Created by Jorge Beltrán on 04/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
