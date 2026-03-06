919a479ee331a4d63795734f75cfb4c988caf7e3
Martin Chacon <chacon0754@outlook.com>
2026-03-05 22:39:51 -0600

    delete: unnecesary files

--- backend/src/enums/-category.enum.ts
+++
@@ -1,9 +0,0 @@
-export enum Category {
-    Culture = "Culture",
-    ScientificPhilosophical = "ScientificPhilosophical",
-    Sports = "Sports",
-    CommunitySupport = "CommunitySupport",
-    Health = "Health",
-    FutureLivingTools = "FutureLivingTools"
-
-}
