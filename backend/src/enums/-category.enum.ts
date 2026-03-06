39a831ed7c6ed4293580153fc48d598debbc4148
Martin Chacon <chacon0754@outlook.com>
2026-03-05 18:22:42 -0600

    feature: create enum for events category

---
+++ backend/src/enums/-category.enum.ts
@@ -0,0 +1,9 @@
+export enum Category {
+    Culture = "Culture",
+    ScientificPhilosophical = "ScientificPhilosophical",
+    Sports = "Sports",
+    CommunitySupport = "CommunitySupport",
+    Health = "Health",
+    FutureLivingTools = "FutureLivingTools"
+
+}
