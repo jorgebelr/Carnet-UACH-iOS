b55cf2a65e17e7040808d4d027a869e6ca33a1ed
Martin Chacon <chacon0754@outlook.com>
2026-03-05 20:22:49 -0600

    Feat: add log to db connection

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,5 +4,6 @@ import { env } from "./env";
 export async function connectDB(): Promise<void> {
     await mongoose.connect(env.MONGODB_URI);
     console.log("Connection to MONGODB made succesfully")
+    console.log("Database: ", mongoose.connection.name);
     
 }
