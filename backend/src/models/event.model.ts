14247b9b25ea89e4277754da43f4028639047cd0
Martin Chacon <chacon0754@outlook.com>
2026-03-05 18:23:01 -0600

    Feature: create interface and model of an event for db

---
+++ backend/src/models/event.model.ts
@@ -0,0 +1,41 @@
+import { Schema, model}  from "mongoose";
+import type { IEvent } from "../interfaces/event.interface";
+import { Category } from "../enums/-category.enum";
+
+
+const eventSchema = new Schema(
+    {
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: Object.values(Category),
+    },
+    description: {
+        type: String,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    maxCapacity: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+    reservedCount: {
+        type: Number,
+        default: 0,
+        min: 0,
+    },
+},
+{
+    timestamps: true
+}
+);
+
+export const EventModel = model<IEvent>("Event", eventSchema)
